68b95e9539cb05a4c62bde44584324aff1f5fa01
Shageesha <shageeshainfo@gmail.com>
2018-10-07 15:42:50 +0530

    changes in recharge interface

--- Ticketing/src/views/recharge/recharge.jsx
+++ Ticketing/src/views/recharge/recharge.jsx
@@ -1,8 +1,9 @@
 import React,{Component} from 'react'
 import '../../css/recharge.css'
-// import Upper from '../upperImage'
 import queryString from 'query-string' ;
 import Getcode from './recharge';
+import CreditCardInput from 'react-credit-card-input';
+import GenerateRandomCode from 'react-random-code-generator';
 
 export default class Reacharge extends Component{
 
@@ -10,10 +11,58 @@ export default class Reacharge extends Component{
         super(props)
         this.state= {
             customerDetails:[],
-            payment : {credit : true, mobile: false}
+            payment : {credit : true, mobile: false},
+           
         }
+        
     }
 
+    setData(e){
+        e.preventDefault();
+        var creditnum = document.getElementById('creditnum').value;
+        var pinnum = document.getElementById('pinnum').value;
+
+        fetch('http://localhost:3005/recharges/postrecharge', {
+            method : 'post',
+            headers : {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body : JSON.stringify({creditnum:creditnum,pinnum: pinnum})
+        }).then(function (data) {
+            return data;
+        }).then(function (confirm) {
+            console.log(confirm);
+            alert('success');
+        })
+    }
+
+    setData01(e){
+        e.preventDefault();
+        var mobilenum = document.getElementById('mobilenum').value;
+        var amount = document.getElementById('amount').value;
+
+        fetch('http://localhost:3005/recharges/postrecharge', {
+            method : 'post',
+            headers : {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body : JSON.stringify({mobilenum:mobilenum,amount: amount})
+        }).then(function (data) {
+            return data;
+        }).then(function (confirm) {
+            console.log(confirm);
+            alert('success');
+        })
+    }
+
+      genetrateCode(){
+        const random= GenerateRandomCode.NumCode(4);
+        alert(random);
+        
+    }
+    
     componentWillMount(){
         const values = queryString.parse(this.props.location.search)
         this.setState({customerDetails:{name:values.customerName,address:values.customerAddress,phone:values.phone}})
@@ -36,6 +85,8 @@ export default class Reacharge extends Component{
 
     }
 
+    
+
     ShowPayment = () => {
         
         if(this.state.payment.credit){
@@ -43,15 +94,25 @@ export default class Reacharge extends Component{
                 <center>
                     <h3>Credit Card Payment</h3>
                         <div class="form-group">
-                            <label>Enter Card No :</label>
-                            <input type="text" class="form-control single"  />
+
+                        <label>Enter Card No :</label>
+                        <CreditCardInput
+                            cardNumberInputProps
+
+                            cardExpiryInputProps
+                            cardCVCInputProps
+                            
+                            fieldClassName="input"
+                        />
+                            {/* <input CreditCardInput cardNumberInputProps class="form-control single" id="creditnum" maxlength="19" placeholder="Card Number"/>
+                             */}
                         </div>
                 
                         <div class="form-group">
                             <label >Enter Card pin :</label>
-                            <input type="text" class="form-control single" id="email" />
+                            <input type="password" class="form-control single" id="pinnum" maxlength="4"/>
                         </div>
-                        <button type="submit" style={{backgroundColor:"gray"}} class="btn btn-default">Submit</button>
+                        <button type="submit" style={{backgroundColor:"gray"}} class="btn btn-default" onClick={this.setData.bind(this)}>Submit</button>
                         {/*<Getcode />*/}
                 </center>
             </div>
@@ -60,13 +121,14 @@ export default class Reacharge extends Component{
                 <div className="phoneText">
                 <div class="form-group">
                         <label >Mobile Number :</label>
-                        <input type="text" class="form-control single" id="email" />
+                        <input type="number" maxLength="10" class="form-control single" id="mobilenum" required/>
                     </div>
                     <div class="form-group">
                         <label>Recharge Amount :</label>
-                        <input type="text" class="form-control single" id="email" />
+                        <input type="text" class="form-control single" id="amount" required/>
                     </div>
-                    <button type="submit" style={{backgroundColor:"gray"}} className="btn btn-default getCode">Get Code</button>
+                   
+                    <button type="submit" style={{backgroundColor:"gray"}} className="btn btn-default getCode" onClick={this.genetrateCode.bind(this)}>Get Code</button>
                 </div>
 
                     <div className="col-sm-12 box" style={{height:"120px"}}>
@@ -75,13 +137,16 @@ export default class Reacharge extends Component{
                                 <td>Please enter the 4 digit code to send to your phone to proceed recharge</td>
                             </tr>
                             <tr>
-                                <td><center> <input type="text" class="form-control single" id="email" /></center></td>
+                                <td><center> <input type="text" class="form-control single" id="code" /></center></td>
                             </tr>
                         </table>
                  </div>
+                 <button type="submit" style={{backgroundColor:"gray"}} class="btn btn-default" onClick={this.setData01.bind(this)}>Confirm payment</button>
             </div>
         }
     }
+
+    
 
     render(){
 
